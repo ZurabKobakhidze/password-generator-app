3012f7eef5e0b913a2d34234527b107a3df791c3
Zukak <zukakobakhidze@gmail.com>
2023-05-04 20:57:44 +0400

    fix: functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
 import "./app.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
   const [isCopied, setIsCopied] = useState(false);
-  const [rangeValue, setRangeValue] = useState(2);
+  const [rangeValue, setRangeValue] = useState(10);
 
   const handleCopyClick = () => {
     setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1200);
   };
-
- 
 
   const handleRangeChange = (event) => {
     setRangeValue(event.target.value);
   };
 
   const getGreenLineWidth = (rangeValue) => {
-    
-    return rangeValue * 25;
+    return ((rangeValue - 5) / 10) * 100;
   };
 
-  const characterNumberMap = {
-    '0': 1,
-    '1': 5,
-    '2': 10,
-    '3': 15,
-    '4': 20,
+  const [activeCheckboxes, setActiveCheckboxes] = useState([
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const handleCheckboxClick = (index) => {
+    const newActiveCheckboxes = [...activeCheckboxes];
+    newActiveCheckboxes[index] = !newActiveCheckboxes[index];
+    setActiveCheckboxes(newActiveCheckboxes);
   };
 
-  const getCharacterNumber = (rangeValue) => {
-    return characterNumberMap[rangeValue] || 0;
+  const getCheckboxButtonStyle = (isActive) => {
+    return isActive
+      ? {
+          backgroundColor: "#A4FFAF",
+          border: "none",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }
+      : {};
   };
 
   return (
@@ -39,53 +52,76 @@ function App() {
       <div className="copy_box">
         <h1 className="copy_numbers">PTx1f5DaFX</h1>
         <div className="logo_div">
-        {isCopied && <p className="copied_text">COPIED</p>}
-
-        <svg
-          className="copy_logo"
-          width="21"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          onClick={handleCopyClick}
-        >
-          <path className="my_path"
-            d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
-            fill="#A4FFAF"
-          />
-          
-        </svg>
-
+          {isCopied && <p className="copied_text">COPIED</p>}
+          <svg
+            className="copy_logo"
+            width="21"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={handleCopyClick}
+          >
+            <path
+              className="my_path"
+              d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
+              fill="#A4FFAF"
+            />
+          </svg>
         </div>
-        
       </div>
       <div className="container">
         <div className="character_box">
           <h2 className="character_length">Character Length</h2>
-          <h2 className="character_number">{getCharacterNumber(rangeValue)}</h2>
+          <h2 className="character_number">{rangeValue}</h2>
         </div>
         <div className="input_box">
-          <input type="range" id="vol" name="vol" min="0" max="4" value={rangeValue} onChange={handleRangeChange} />
+          <input
+            type="range"
+            id="vol"
+            name="vol"
+            min="5"
+            max="15"
+            value={rangeValue}
+            onChange={handleRangeChange}
+          />
           <output></output>
-          <div class="green_line" style={{ width: `${getGreenLineWidth(rangeValue)}%` }}></div>
+          <div
+            class="green_line"
+            style={{ width: `${getGreenLineWidth(rangeValue)}%` }}
+          ></div>
         </div>
         <div className="checkbox_div">
-          <div className="checkbox_label_box">
-           <button className="checkbox_button"></button>
-            <span class="checkbox_text">Include Uppercase Letters</span>
-          </div>
-          <div className="checkbox_label_box">
-          <button className="checkbox_button"></button>
-            <span class="checkbox_text">Include Lowercase Letters</span>
-          </div>
-          <div className="checkbox_label_box">
-          <button className="checkbox_button"></button>
-            <span class="checkbox_text">Include Numbers</span>
-          </div>
-          <div className="checkbox_label_box">
-          <button className="checkbox_button"></button>
-            <span class="checkbox_text">Include Symbols</span>
-          </div>
+          {[
+            "Include Uppercase Letters",
+            "Include Lowercase Letters",
+            "Include Numbers",
+            "Include Symbols",
+          ].map((text, index) => (
+            <div className="checkbox_label_box" key={index}>
+              <button
+                className="checkbox_button"
+                style={getCheckboxButtonStyle(activeCheckboxes[index])}
+                onClick={() => handleCheckboxClick(index)}
+              >
+                {activeCheckboxes[index] && (
+                  <svg
+                    width="14"
+                    height="12"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke="#18171F"
+                      stroke-width="3"
+                      fill="none"
+                      d="M1 5.607 4.393 9l8-8"
+                    />
+                  </svg>
+                )}
+              </button>
+              <span class="checkbox_text">{text}</span>
+            </div>
+          ))}
         </div>
+
         <div className="strength_box">
           <h2 className="strength_h2">STRENGTH</h2>
           <div className="strength_power">
@@ -98,7 +134,15 @@ function App() {
             </div>
           </div>
         </div>
-        <button className="generate_button">GENERATE <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg></button>
+        <button className="generate_button">
+          GENERATE{" "}
+          <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill="#24232C"
+              d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
